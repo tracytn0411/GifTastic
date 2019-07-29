da7815f8a17ca4488de56195b99c8cc5fad05fd8
TracyTN <tracytn0411@gmail.com>
2019-07-29 15:26:29 -0700

    Add form to take user input.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -5,6 +5,8 @@ var topics = ["The Office", "Silicon Valley", "South Park", "Arrested Developmen
 displayButton();
 
 function displayButton() {
+    
+
     $.each (topics, function() {
         var button = $("<button>").addClass("btn btn-primary").text(this).attr({
             "id" : "tvshow",
@@ -12,13 +14,17 @@ function displayButton() {
         })
         $("#topicDisplay").append(button);
     });
+}
 
-    $(".btn-primary").on("click", function(event) {
-        event.preventDefault();
-        
+$(document).on("click", ".btn-primary", function() {
+        //event.preventDefault();
+        $("#resultDisplay").empty(); //empty content so only 10 gifs display at a time
+
         var tvTitle = $(this).attr("tv_title");
         console.log(tvTitle);
     
+        //limit = 10 -> max 10 gifs display
+        //rating = G -> general rating, good for all ages
         var u = "https://api.giphy.com/v1/gifs/search?api_key=fDUfRFjS2TdWvnNC2NYYCXaUXXv3VyAr&q=" + tvTitle + "&limit=10&rating=G&lang=en"; 
         console.log(u);
     
@@ -26,16 +32,21 @@ function displayButton() {
             url: u,
             method: "GET"
         }).done(function (response) {
-            console.log(response.data);
+            //console.log(response.data);
             var tvObs = response.data;
     
-            $.each(tvObs, function(){
-                var divGif = $("<div>").addClass("gify");
+            $.each(tvObs, function(){ //this = each tvObs 
+                var divGif = $("<div>").addClass("col-4 gify");
                 var displayRating = $("<p>").html("Rating: " + this.rating);
     
-                var gifImg = $("<img>").addClass("gif").attr({
+                var gifImg = $("<img>").addClass("img-fluid gif").attr({
                     "src" : this.images.fixed_height_still.url,
-                })
+                    "data_still" : this.images.fixed_height_still.url,
+                    "data_animate" : this.images.fixed_height.url,
+                    "data_state" : "still",
+                });
+
+                //console.log(this.images.fixed_height_still.url)
     
                 divGif.append(displayRating);
                 divGif.prepend(gifImg);
@@ -43,7 +54,32 @@ function displayButton() {
                 $("#resultDisplay").append(divGif);
             });
         });
-    })
-}
+    });
+
+$("#submit_btn").on("click", function(event){
+    event.preventDefault();
+    $("#topicDisplay").empty();
+    var newTvshow = $("#userInput").val().trim();
+    console.log(newTvshow);
+    topics.push(newTvshow);
+    console.log(topics);
+    displayButton();
+})
+
+
+    //! $(."gif").on("click", function())} won't work here
+$(document).on("click", ".gif", function (){
+    var gifState = $(this).attr("data_state");
+
+    if (gifState === "still"){
+         $(this).attr("src", $(this).attr("data_animate"));
+        $(this).attr("data_state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data_still"));
+        $(this).attr("data_state", "still");
+    }
+
+
+})
 
 })
